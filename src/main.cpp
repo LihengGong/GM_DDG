c3f64047ba3bf8f8697e745799cad8acc83a5e53
Liheng.Gong <liheng.gong@hotmail.com>
2019-05-23 21:38:50 -0400

    init commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,34 +1,50 @@
 #include <igl/opengl/glfw/Viewer.h>
+#include <igl/readOFF.h>
+#include <igl/per_vertex_normals.h>
+#include <igl/per_face_normals.h>
+#include <igl/per_corner_normals.h>
+
+Eigen::MatrixXd V;
+Eigen::MatrixXi F;
+
+Eigen::MatrixXd N_vertices;
+Eigen::MatrixXd N_faces;
+Eigen::MatrixXd N_corners;
+
+bool key_down(igl::opengl::glfw::Viewer& viewer, unsigned char key, int modifier) {
+  switch (key) {
+    case '1':
+      viewer.data().set_normals(N_faces);
+      return true;
+    case '2':
+      viewer.data().set_normals(N_vertices);
+      return true;
+    case '3':
+      viewer.data().set_normals(N_corners);
+      return true;
+    default: break;
+  }
+
+  return false;
+}
 
 int main(int argc, char *argv[])
 {
-  // Inline mesh of a cube
-  const Eigen::MatrixXd V= (Eigen::MatrixXd(8,3)<<
-    0.0,0.0,0.0,
-    0.0,0.0,1.0,
-    0.0,1.0,0.0,
-    0.0,1.0,1.0,
-    1.0,0.0,0.0,
-    1.0,0.0,1.0,
-    1.0,1.0,0.0,
-    1.0,1.0,1.0).finished();
-  const Eigen::MatrixXi F = (Eigen::MatrixXi(12,3)<<
-    1,7,5,
-    1,3,7,
-    1,4,3,
-    1,2,4,
-    3,8,7,
-    3,4,8,
-    5,7,8,
-    5,8,6,
-    1,5,6,
-    1,6,2,
-    2,6,8,
-    2,8,4).finished().array()-1;
+  std::string file_name = argv[1];
+  igl::readOFF(file_name, V, F);
+
+  igl::per_face_normals(V, F, N_faces);
+
+  igl::per_vertex_normals(V, F, N_vertices);
+
+  igl::per_corner_normals(V, F, 20, N_corners);
 
   // Plot the mesh
   igl::opengl::glfw::Viewer viewer;
+  viewer.callback_key_down = &key_down;
+  viewer.data().show_lines = false;
   viewer.data().set_mesh(V, F);
-  viewer.data().set_face_based(true);
+  viewer.data().set_normals(N_faces);
+
   viewer.launch();
 }
